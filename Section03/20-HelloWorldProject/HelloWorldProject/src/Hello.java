e4d79b4e9efbb731614b13da65d1a22487510cd4
FilipeFariaas <filipefarias117@gmail.com>
2022-09-04 14:20:40 -0300

    create main method

--- Section03/20-HelloWorldProject/HelloWorldProject/src/Hello.java
+++ Section03/20-HelloWorldProject/HelloWorldProject/src/Hello.java
@@ -1,2 +1,10 @@
 public class Hello {
+    /*
+    public - access modifier
+    static - needed for Java to find the method to run the code that will be added
+    void - indicates that the method will not return anything
+    */
+    public static void main(String[] args) {
+
+    }
 }
