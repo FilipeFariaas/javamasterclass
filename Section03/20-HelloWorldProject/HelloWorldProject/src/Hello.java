0a10237315eb4b2cd3e931f21c7faa7ec45b636a
FilipeFariaas <filipefarias117@gmail.com>
2022-09-04 14:28:52 -0300

    add the prtin statement to main method

--- Section03/20-HelloWorldProject/HelloWorldProject/src/Hello.java
+++ Section03/20-HelloWorldProject/HelloWorldProject/src/Hello.java
@@ -2,9 +2,9 @@ public class Hello {
     /*
     public - access modifier
     static - needed for Java to find the method to run the code that will be added
-    void - indicates that the method will not return anything
+    void   - indicates that the method will not return anything
     */
     public static void main(String[] args) {
-
+        System.out.println("Hello World"); // <- statement - a complete command to be executed
     }
 }
