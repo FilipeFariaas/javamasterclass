22341d9be8c6118a8b9c98a380d4eed36b2ad6b4
FilipeFariaas <filipefarias117@gmail.com>
2022-09-04 15:06:45 -0300

    add myThirdNumber variable

--- Section03/23-Variables/Variables/src/Main.java
+++ Section03/23-Variables/Variables/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         */
         int myFirstNumber = (10 + 5) + (2 * 10); // <- it is an expression. A construct that evaluates to a single value
         int mySecondNumber = 12;
+        int myThirdNumber = 6;
 
         System.out.println(myFirstNumber);
     }
