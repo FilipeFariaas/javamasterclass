3f4b94102220d056d497132a6ea028e320e09dc0
FilipeFariaas <filipefarias117@gmail.com>
2022-09-04 15:05:35 -0300

    converts myFirstNumber into a more complex expression

--- Section03/23-Variables/Variables/src/Main.java
+++ Section03/23-Variables/Variables/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         =             - means that the value at it's right will be stored into the variable at it's left side
         5             - is the value to be stored
         */
-        int myFirstNumber = 5; // <- it is an expression. A construct that evaluates to a single value
+        int myFirstNumber = (10 + 5) + (2 * 10); // <- it is an expression. A construct that evaluates to a single value
 
         System.out.println(myFirstNumber);
     }
