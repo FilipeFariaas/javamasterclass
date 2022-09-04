9d876aa6582ef632212f97e5e4e15343ae73ed55
FilipeFariaas <filipefarias117@gmail.com>
2022-09-04 14:32:31 -0300

    change statement sentence to 'Hello, Filipe'

--- Section03/20-HelloWorldProject/HelloWorldProject/src/Hello.java
+++ Section03/20-HelloWorldProject/HelloWorldProject/src/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
     void   - indicates that the method will not return anything
     */
     public static void main(String[] args) {
-        System.out.println("Hello World"); // <- statement - a complete command to be executed
+        System.out.println("Hello, Filipe"); // <- statement - a complete command to be executed
     }
 }
