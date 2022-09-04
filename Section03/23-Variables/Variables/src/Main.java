06ab6cc6b843aec287adbdb57b864cb3b076b280
FilipeFariaas <filipefarias117@gmail.com>
2022-09-04 14:49:05 -0300

    create a int variable

--- Section03/23-Variables/Variables/src/Main.java
+++ Section03/23-Variables/Variables/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        /*
+        int           - is the variable type, in this case "int" (integer number)
+        myFirstNumber - is the variable name
+        =             - means that the value at it's right will be stored into the variable at it's left side
+        5             - is the value to be stored
+        */
+        int myFirstNumber = 5;
     }
 }
