4d9ba0964efcab688e4e5ddabffcde771cbd09db
FilipeFariaas <filipefarias117@gmail.com>
2022-09-11 15:09:49 -0300

    add casting example

--- Section03/27-Casting/Casting/src/Main.java
+++ Section03/27-Casting/Casting/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("----- CASTING -----");
+        // casting converts a value from one type to another
+
+        byte minByteValue = Byte.MIN_VALUE;
+
+        // byte halfMinValue = (minByteValue / 2); <-- java understands this expression as an int value
+
+        // casting
+        byte halfMinValue = (byte) (minByteValue / 2); // <-- converts the expression to byte
+        System.out.println(halfMinValue);
     }
 }
