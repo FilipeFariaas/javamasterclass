f9c6c042f0fad58d365ff94530612ca0d1ca6e11
FilipeFariaas <filipefarias117@gmail.com>
2022-09-11 14:41:23 -0300

    add byte, short and long types

--- Section03/25-PrimitiveTypes/PrimitiveTypes/src/udemy/javamasterclass/Main.java
+++ Section03/25-PrimitiveTypes/PrimitiveTypes/src/udemy/javamasterclass/Main.java
@@ -16,10 +16,43 @@ public class Main {
         /* Integer is a Wrapper Class
            Java has Wrapper Classes for all eight primitive types
         */
+
+        System.out.println("\n----- INT -----");
+
         int minimumIntValue = Integer.MIN_VALUE;
         int maximumIntValue = Integer.MAX_VALUE;
 
         System.out.println("Minimum Integer Value: " + minimumIntValue);
         System.out.println("Maximum Integer Value: " + maximumIntValue);
+        System.out.println("Underflow Integer Value: (minimum value - 1): " + (minimumIntValue - 1));
+        System.out.println("Overflow Integer Value: (maximum value + 1): " + (maximumIntValue + 1));
+
+        // int values can be separated by "_"
+        int separatedIntValue = 2_123_456;
+        System.out.println("Integer Value Separated by '_' (2_123_456) number: " + separatedIntValue);
+
+        System.out.println("\n----- BYTE -----");
+
+        byte minimumByteValue = Byte.MIN_VALUE;
+        byte maximumByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Minimum Byte Value: " + minimumByteValue);
+        System.out.println("Maximum Byte Value: " + maximumByteValue);
+
+        System.out.println("\n----- SHORT -----");
+
+        short minimumShortValue = Short.MIN_VALUE;
+        short maximumShortValue = Short.MAX_VALUE;
+
+        System.out.println("Minimum Short Value: " + minimumShortValue);
+        System.out.println("Maximum Short Value: " + maximumShortValue);
+
+        System.out.println("\n----- LONG -----");
+
+        long minimumLongValue = Long.MIN_VALUE;
+        long maximumLongValue = Long.MAX_VALUE;
+
+        System.out.println("Minimum Long Value: " + minimumLongValue);
+        System.out.println("Maximum Long Value: " + maximumLongValue);
     }
 }
