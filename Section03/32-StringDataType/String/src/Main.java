c8c71d738509b0dd0afd8c4591d6da689675092a
FilipeFariaas <filipefarias117@gmail.com>
2022-09-11 17:55:09 -0300

    add examples

--- Section03/32-StringDataType/String/src/Main.java
+++ Section03/32-StringDataType/String/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("----- STRING -----");
+        // stores a full sequence of chars between double quotes (a text)
+        // not a primitive type, but a class
+
+        String stringVal = "This is a string";
+        System.out.println(stringVal);
+
+        // a String can be combined with another strings
+        stringVal += ". \u00A9 2022";
+        System.out.println(stringVal);
+
+        // Java will automatically convert any value combined to a String into a String
+        String numberString = "10";
+        int numberInt = 10;
+
+        System.out.println(numberString + numberInt);
     }
 }
